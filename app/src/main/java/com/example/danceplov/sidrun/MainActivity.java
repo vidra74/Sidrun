1179501cd0c3791ee632ece3fc05df817f3031e8
vidra74 <fjancic@inet.hr>
2015-07-29 10:59:17 +0200

    New transparent logo. App icon on title bar.

--- app/src/main/java/com/example/danceplov/sidrun/MainActivity.java
+++ app/src/main/java/com/example/danceplov/sidrun/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
     }
 
