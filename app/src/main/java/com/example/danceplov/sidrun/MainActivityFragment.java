4df2ad6d6b6c74ce670d4f6af15e952b5fa970f2
vidra74 <fjancic@inet.hr>
2015-07-30 16:21:17 +0200

    New activity for stadium list view. Implemented button to start activity.

--- app/src/main/java/com/example/danceplov/sidrun/MainActivityFragment.java
+++ app/src/main/java/com/example/danceplov/sidrun/MainActivityFragment.java
@@ -1,10 +1,14 @@
 package com.example.danceplov.sidrun;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 /**
@@ -12,12 +16,31 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    ImageButton mStadioniButton;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+/*
+        ;*/
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mStadioniButton = (ImageButton)getView().findViewById(R.id.ibStadioni);
+        mStadioniButton.setOnClickListener(new ImageButton.OnClickListener() {
+               @Override
+               public void onClick(View arg0) {
+                   Intent browserIntent = new Intent(getActivity(), StadionActivity.class);
+                   startActivity(browserIntent);
+               }
+           }
+        );
     }
 }
